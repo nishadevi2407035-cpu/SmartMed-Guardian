41bc583b833c2971fc333eec787816dd96d3b679
nishadevi2407035-cpu <nishadevi.2407035@srec.ac.in>
2026-01-01 01:51:21 +0530

    Add placeholder code for sound sensor

---
+++ sensor_code.ino
@@ -0,0 +1,10 @@
+// Placeholder code for sound sensor
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Smart Infant Care Sensor Placeholder");
+}
+
+void loop() {
+  // Read sound sensor
+  // Send data to cloud / microcontroller
+}
